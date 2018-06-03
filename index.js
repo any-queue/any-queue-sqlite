cb9142ecfdae20ab7ae465449b7c4d397eda1836
Gerardo Munguia <gerardomunguiahoyo@gmail.com>
2018-06-03 13:06:27 +0200

    Move connection variables to parameters

--- index.js
+++ index.js
@@ -1,44 +1,42 @@
 const mysql = require("mysql");
 const { promisify } = require("util");
 
-// TODO: let user supply connection params.
-const config = {
-  host: "localhost",
-  rootUser: "root",
-  rootPassword: "admin",
-  user: "one_queue_test_user",
-  password: "one_queue_test_password",
-  database: "one_queue_test"
-};
+module.exports = ({
+  host,
+  rootUser,
+  rootPassword,
+  user,
+  password,
+  database
+}) => {
+  const globals = {};
 
-const globals = {};
+  const initialize = function initialize() {
+    const connection = mysql.createConnection({
+      host: host,
+      user: rootUser,
+      password: rootPassword
+    });
 
-const initialize = function initialize() {
-  const connection = mysql.createConnection({
-    host: config.host,
-    user: config.rootUser,
-    password: config.rootPassword
-  });
-
-  return promisify(connection.connect.bind(connection))()
-    .then(() => promisify(connection.query.bind(connection)))
-    .then(query =>
-      query(`DROP DATABASE IF EXISTS ${config.database}`)
-        .then(query(`CREATE DATABASE ${config.database}`))
-        .then(() =>
-          query(`CREATE USER IF NOT EXISTS '${config.user}'@'${config.host}'`)
-        )
-        .then(() =>
-          query(
-            `GRANT ALL ON ${config.database}.* To '${config.user}'@'${
-              config.host
-            }' IDENTIFIED BY '${config.password}';`
+    return promisify(connection.connect.bind(connection))()
+      .then(() => promisify(connection.query.bind(connection)))
+      .then(query =>
+        query(`DROP DATABASE IF EXISTS ${database}`)
+          .then(query(`CREATE DATABASE ${database}`))
+          .then(() =>
+            query(`CREATE USER IF NOT EXISTS '${user}'@'${host}'`)
           )
-        )
-        .then(() => query("FLUSH PRIVILEGES"))
-        .then(() => query(`USE ${config.database}`))
-        .then(() =>
-          query(`CREATE TABLE jobs(
+          .then(() =>
+            query(
+              `GRANT ALL ON ${database}.* To '${user}'@'${
+                host
+              }' IDENTIFIED BY '${password}';`
+            )
+          )
+          .then(() => query("FLUSH PRIVILEGES"))
+          .then(() => query(`USE ${database}`))
+          .then(() =>
+            query(`CREATE TABLE jobs(
           _id int not null primary key auto_increment,
           queue varchar(255) not null,
           data json not null,
@@ -46,9 +44,9 @@ const initialize = function initialize() {
           status enum ('new', 'blocked', 'done', 'failed') not null,
           error varchar(1000)
         )`)
-        )
-        .then(() =>
-          query(`CREATE TABLE locks(
+          )
+          .then(() =>
+            query(`CREATE TABLE locks(
           _id int not null primary key auto_increment,
           queue varchar(255) not null,
           worker char(36),
@@ -61,110 +59,111 @@ const initialize = function initialize() {
           ON UPDATE CASCADE
           ON DELETE CASCADE
         )`)
-        )
-    )
-    .then(promisify(connection.end.bind(connection)));
-};
+          )
+      )
+      .then(promisify(connection.end.bind(connection)));
+  };
 
-const connect = function connect() {
-  const connection = mysql.createConnection({
-    host: config.host,
-    user: config.user,
-    password: config.password,
-    database: config.database
-  });
+  const connect = function connect() {
+    const connection = mysql.createConnection({
+      host: host,
+      user: user,
+      password: password,
+      database: database
+    });
 
-  return promisify(connection.connect.bind(connection))()
-    .then(() => promisify(connection.query.bind(connection)))
-    .then(query => {
-      const formatValue = function formatValue(value) {
-        switch (typeof value) {
-          case "number":
-            return value;
-          case "string":
-            return `'${value}'`;
-          case "undefined":
-            return "null";
-          default:
-            return `'${JSON.stringify(value)}'`;
-        }
-      };
+    return promisify(connection.connect.bind(connection))()
+      .then(() => promisify(connection.query.bind(connection)))
+      .then(query => {
+        const formatValue = function formatValue(value) {
+          switch (typeof value) {
+            case "number":
+              return value;
+            case "string":
+              return `'${value}'`;
+            case "undefined":
+              return "null";
+            default:
+              return `'${JSON.stringify(value)}'`;
+          }
+        };
 
-      const wheres = function wheres(obj = {}) {
-        const conditions = Object.keys(obj)
-          .map(k => `${k} = ${formatValue(obj[k])}`)
-          .join(" AND ");
+        const wheres = function wheres(obj = {}) {
+          const conditions = Object.keys(obj)
+            .map(k => `${k} = ${formatValue(obj[k])}`)
+            .join(" AND ");
 
-        return conditions ? `WHERE ${conditions}` : "";
-      };
+          return conditions ? `WHERE ${conditions}` : "";
+        };
 
-      const environment = {
-        createJob: function createJob({ queue, data, priority, status }) {
-          return query(`
+        const environment = {
+          createJob: function createJob({ queue, data, priority, status }) {
+            return query(`
           INSERT INTO jobs(queue, data, priority, status)
           VALUES('${queue}', '${JSON.stringify(
-            data
-          )}', ${priority}, '${status}')
+              data
+            )}', ${priority}, '${status}')
           `).then(({ insertId }) => insertId);
-        },
-        readJob: function readJob(criteria) {
-          return query(`
+          },
+          readJob: function readJob(criteria) {
+            return query(`
           SELECT * FROM jobs 
           ${wheres(criteria)}
           `).then(jobs =>
-            jobs.map(j => Object.assign({}, j, { data: JSON.parse(j.data) }))
-          );
-        },
-        updateJob: function updateJob(id, { status, error }) {
-          return query(`
+              jobs.map(j => Object.assign({}, j, { data: JSON.parse(j.data) }))
+            );
+          },
+          updateJob: function updateJob(id, { status, error }) {
+            return query(`
           UPDATE jobs
           SET status = '${status}' ${error ? `, error = ${error}` : ""}
           WHERE _id = ${id}
           `);
-        },
-        createLock: function createLock({
-          job,
-          queue,
-          worker,
-          status,
-          blocker
-        }) {
-          return query(`
+          },
+          createLock: function createLock({
+            job,
+            queue,
+            worker,
+            status,
+            blocker
+          }) {
+            return query(`
           INSERT INTO locks(job, queue, worker, blocker, status)
           VALUES(${job}, '${queue}', ${formatValue(worker)}, ${formatValue(
-            blocker
-          )}, '${status}')
+              blocker
+            )}, '${status}')
           `).then(({ insertId }) => insertId);
-        },
-        readLock: function readLock(criteria) {
-          return query(`
+          },
+          readLock: function readLock(criteria) {
+            return query(`
           SELECT * FROM locks 
           ${wheres(criteria)}
           `);
-        },
-        updateLock: function updateLock(criteria, { status }) {
-          return query(`
+          },
+          updateLock: function updateLock(criteria, { status }) {
+            return query(`
           UPDATE locks
           SET status = '${status}'
           ${wheres(criteria)}
           `);
-        }
-      };
+          }
+        };
 
-      globals.connection = connection;
-      globals.query = query;
+        globals.connection = connection;
+        globals.query = query;
 
-      return environment;
-    });
+        return environment;
+      });
+  };
+
+  const refresh = function() {
+    return globals.query("DELETE FROM jobs");
+  };
+
+  const disconnect = function() {
+    const connection = globals.connection;
+    return promisify(connection.end.bind(connection))();
+  };
+
+  return { initialize, connect, refresh, disconnect };
 };
-
-const refresh = function() {
-  return globals.query("DELETE FROM jobs");
-};
-
-const disconnect = function() {
-  const connection = globals.connection;
-  return promisify(connection.end.bind(connection))();
-};
-
-module.exports = { initialize, connect, refresh, disconnect };
--- test.js
+++ test.js
@@ -1,5 +1,13 @@
 "use strict";
 
 const testIntegration = require("../any-queue-test/index.js");
-const { connect, initialize, refresh, disconnect } = require(".");
+const { connect, initialize, refresh, disconnect } = require(".")({
+  host: "localhost",
+  rootUser: "root",
+  rootPassword: "admin",
+  user: "any_queue_test_user",
+  password: "any_queue_test_password",
+  database: "any_queue_test"
+});
+
 testIntegration({ name: "mysql", connect, initialize, refresh, disconnect });
